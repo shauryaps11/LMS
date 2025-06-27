b2dc05e5074e9a38c13cbebba4e7c50ad788eb82
Shaurya Pratap Singh <shaurya27pratapsingh@gmail.com>
2025-06-27 11:33:06 +0000

    updated

--- client/src/components/student/Hero.jsx
+++ client/src/components/student/Hero.jsx
@@ -3,6 +3,7 @@ import { assets } from '../../assets/assets';
 import SearchBar from '../../components/student/SearchBar';
 
 const Hero = () => {
+// hero to be linked with home
   return (
     <div className="flex flex-col items-center justify-center w-full md:pt-36 pt-20 px-7 md:px-0 space-y-7 text-center bg-gradient-to-b from-cyan-100/70">
       <h1 className="md:text-home-heading-large text-home-heading-small relative font-bold text-gray-800 max-w-3xl mx-auto">
